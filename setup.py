6f46b0416ec5d9dc85e1afba563ffd95c97929a1
Chase Minert <chaseminert@u.boisestate.edu>
2024-06-01 14:52:39 -0600

    Setup actually works

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    description = f.read()
 
-if __name__ == "__main__":
-    main()
+setup(
+    name='easymp3',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'mutagen==1.47.0'
+    ],
+    author="Chase Minert",
+    author_email="cminert58@gmail.com",
+    long_description=description,
+)
